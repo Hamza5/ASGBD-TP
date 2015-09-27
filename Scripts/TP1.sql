74b714243d03f634de7bf86c8c7d6582b0c54e5f
Hamza Abbad <hamza.abbad@gmail.com>
2015-09-27 21:36:26 +0100

    Created the files and added the SQL requests.
    Added the SQL requests of the part 1 of the TP1 and initialized the rapport.

---
+++ Scripts/TP1.sql
@@ -0,0 +1,8 @@
+ /* Partie 1 */
+ CREATE TABLESPACE Abbad_tbs '/home/hamza/Documents/Study/M1/S1/ASGBD/TP/Abbad_tbs.dat' SIZE 5M AUTOEXTEND ON ONLINE;
+ CREATE TEMPORARY TABLESPACE Abbad__temp_tbs TEMPFILE '/home/hamza/Documents/Study/M1/S1/ASGBD/TP/Abbad_tbs_temp.dat' SIZE 5M AUTOEXTEND ON;
+ CREATE USER Abbad IDENTIFIED BY AZERTY DEFAULT TABLESPACE Abbad_tbs TEMPORARY TABLESPACE Abbad__temp_tbs;
+ GRANT ALL PRIVILEGES TO Abbad;
+ 
+ /* Partie 2 */
+ 
